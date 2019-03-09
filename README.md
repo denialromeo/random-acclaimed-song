7b33bb6f200111e31de7a2b1cb81e2484288f7a7
Daniel Moore <hi@danielmoore.us>
2019-03-09 05:26:59 -0800

    4 API keys for 4 times the fun! Now a month-long affair. Not as bad.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ YouTube's Search API has a daily usage limit of 100 requests per day. Since ther
 Doing this manually is not my idea of a good time, so I guess I'll just have to be patient. I'll get some new songs to listen to each day, which helps, and I guess I could automate this.
 
 I should probably bail on Google API's for borders-quiz. OpenStreetMap, here I come!
+
+### To-Do
+
+* Delete from `to-write` and append to `written` automatically.
+* Set up schtasks to run script with each API key every day so I don't have to.
--- write-youtube-ids.py
+++ write-youtube-ids.py
@@ -5,6 +5,8 @@ import requests
 def get_youtube_id(search_string):
     # API_KEY = "AIzaSyBQnISjzNNGwITiZ9IGa8h-ACv-zFcQnZw"
     # API_KEY = "AIzaSyD708scD_-j8jkICMSLnhWl9wTDMEN9w3c"
+    # API_KEY = "AIzaSyD6aJcm4_kvngKdzyh59yOZ_5dM_2NwSvc"
+    # API_KEY = "AIzaSyBXIjogxwr82ek2VYgEg9RHt3sRP4Ekf58"
     payload = {"part": "snippet", "key": API_KEY, "q": search_string, "maxResults": 1}
     response = requests.get("https://www.googleapis.com/youtube/v3/search", payload).text
     return json.loads(response)["items"][0]["id"]["videoId"]
