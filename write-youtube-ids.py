ee0223b9ededb31cc3b13fe085c7158d438c35ee
Daniel Moore <hi@danielmoore.us>
2019-03-09 20:16:36 -0800

    Looking good! Just need to test this come midnight before pushing.

--- write-youtube-ids.py
+++ write-youtube-ids.py
@@ -1,26 +1,42 @@
-import fileinput
+import csv
 import json
 import requests
 
+INFILE  = "all-songs"
+OUTFILE = "written"
+
+api_keys = ["AIzaSyBQnISjzNNGwITiZ9IGa8h-ACv-zFcQnZw",
+            "AIzaSyD708scD_-j8jkICMSLnhWl9wTDMEN9w3c",
+            "AIzaSyD6aJcm4_kvngKdzyh59yOZ_5dM_2NwSvc",
+            "AIzaSyBXIjogxwr82ek2VYgEg9RHt3sRP4Ekf58",
+            "AIzaSyAX5l_PLHVgw1OC-hepf2SHgSIDC-2uUlE",
+            "AIzaSyBUY4lBhusyDjODp0o6dfJNAg3gxmpDIe4",
+            "AIzaSyAES65sOUOS22G4XD5ink6EBEoIAN-WUmo"]
+
 def get_youtube_id(search_string):
-    # API_KEY = "AIzaSyBQnISjzNNGwITiZ9IGa8h-ACv-zFcQnZw"
-    # API_KEY = "AIzaSyD708scD_-j8jkICMSLnhWl9wTDMEN9w3c"
-    # API_KEY = "AIzaSyD6aJcm4_kvngKdzyh59yOZ_5dM_2NwSvc"
-    # API_KEY = "AIzaSyBXIjogxwr82ek2VYgEg9RHt3sRP4Ekf58"
-    # API_KEY = "AIzaSyAX5l_PLHVgw1OC-hepf2SHgSIDC-2uUlE"
-    # API_KEY = "AIzaSyBUY4lBhusyDjODp0o6dfJNAg3gxmpDIe4"
-    # API_KEY = "AIzaSyAES65sOUOS22G4XD5ink6EBEoIAN-WUmo"
-    payload = {"part": "snippet", "key": API_KEY, "q": search_string, "maxResults": 1, "type": "video"}
-    response = requests.get("https://www.googleapis.com/youtube/v3/search", payload).text
+    payload = {"part": "snippet", "key": api_keys[-1], "q": search_string, "maxResults": 1, "type": "video"}
+    try:
+        response = requests.get("https://www.googleapis.com/youtube/v3/search", payload).text
+    except KeyError:
+        payload["key"] = api_keys.pop()
+        response = requests.get("https://www.googleapis.com/youtube/v3/search", payload).text
     return json.loads(response)["items"][0]["id"]["videoId"]
 
-def write_youtube_ids():
-    for line in fileinput.input("to-write", inplace=True):
-        line = line.rstrip('\n')
-        values = line.split("|")
-        search_string = str.join(" ", values[0:2])
-        youtube_id = get_youtube_id(search_string)
-        print(line + "|" + youtube_id)
+def num_songs_processed():
+    with open(OUTFILE) as f:
+        return sum(1 for line in f)
+
+def write_to_output(row):
+    with open(OUTFILE, "a") as csvfile:
+        output_writer = csv.writer(csvfile, delimiter="|", lineterminator="\n")
+        output_writer.writerow(row)
 
 if __name__ == "__main__":
-    write_youtube_ids()
+    num_songs_written = num_songs_processed()
+    with open(INFILE) as csvfile:
+        input_reader = csv.reader(csvfile, delimiter="|")
+        for idx, row in enumerate(input_reader):
+            if (idx >= num_songs_written):
+                search_string = str.join(" ", row[0:2])
+                row.append(search_string)
+                write_to_output(row)
