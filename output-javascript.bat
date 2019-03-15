b606adb0f3f8e067f8f869e5b4d40922e3d5842e
Daniel Moore <hi@danielmoore.us>
2019-03-15 13:25:55 -0700

    Output Javascript now UTF-8 encoded so non-ASCII characters rendered properly.

--- output-javascript.bat
+++ output-javascript.bat
@@ -1,6 +1,8 @@
 @echo const songs = [ > songs.js
 @sed s/\"/\\\\\"/g written > t
-@sed "s/^\(.*\)$/\"\1\",/" t >> songs.js
+@powershell "get-content .\t | set-content -encoding utf8 g"
+@sed "s/^\(.*\)$/\"\1\",/" g >> songs.js
 @echo ] >> songs.js
 @echo console.log(songs) >> songs.js
 @del t
+@del g
