1d697e0f7c22d33680ec826b2dddfe27714cdcc3
Daniel Moore <hi@danielmoore.us>
2019-03-16 04:52:02 -0700

    Added and tested a few methods that return if a video exists, is playable in the US, or is embeddable. Key for maintenance.

--- main.py
+++ main.py
@@ -17,33 +17,64 @@ api_keys = ["AIzaSyBQnISjzNNGwITiZ9IGa8h-ACv-zFcQnZw",
             "AIzaSyBUY4lBhusyDjODp0o6dfJNAg3gxmpDIe4",
             "AIzaSyAES65sOUOS22G4XD5ink6EBEoIAN-WUmo"]
 
-def get_response(payload):
-    return json.loads(requests.get("https://www.googleapis.com/youtube/v3/search", payload).text)
+def poll_api(url, payload):
+    return json.loads(requests.get(url, payload).text)
 
-def get_youtube_id(search_string):
-    payload = {"part": "snippet", "key": api_keys[-1], "q": search_string, "maxResults": 1, "type": "video", "regionCode": "US", "videoEmbeddable": "true"}
-    response = get_response(payload)
+def get_response(url, payload):
+    payload["key"] = api_keys[-1]
+    response = poll_api(url, payload)
     while "error" in response:
         print(response["error"]["errors"][0]["message"][0:76])
         api_keys.pop()
         payload["key"] = api_keys[-1]
-        response = get_response(payload)
-    print(response["items"][0]["snippet"]["title"])
+        response = poll_api(url, payload)
+    return response
+
+def get_youtube_id(search_string):
+    url = "https://www.googleapis.com/youtube/v3/search"
+    payload = {"part": "snippet", "q": search_string, "maxResults": 1, "type": "video", "regionCode": "US", "videoEmbeddable": "true"}
+    response = get_response(url, payload)
     return response["items"][0]["id"]["videoId"]
 
-def num_songs_processed():
-    with open(OUTFILE) as f:
+def video_exists(id):
+    url = "https://www.googleapis.com/youtube/v3/videos"
+    payload = {"part": "contentDetails", "id": id}
+    response = get_response(url, payload)
+    return (len(response["items"]) > 0)
+
+def video_is_playable_in_US(id):
+    url = "https://www.googleapis.com/youtube/v3/videos"
+    payload = {"part": "contentDetails", "id": id}
+    response = get_response(url, payload)
+    contentDetails = response["items"][0]["contentDetails"]
+    if "regionRestriction" in contentDetails:
+        if "blocked" in contentDetails["regionRestriction"]:
+            if "US" in contentDetails["regionRestriction"]["blocked"]:
+                return False
+        elif "allowed" in contentDetails["regionRestriction"]:
+            if "US" in contentDetails["regionRestriction"]["allowed"]:
+                return True
+    return True
+
+def video_is_embeddable(id):
+    url = "https://www.googleapis.com/youtube/v3/videos"
+    payload = {"part": "status", "id": id}
+    response = get_response(url, payload)
+    return response["items"][0]["status"]["embeddable"]
+
+def num_lines_in_file(file):
+    with open(file) as f:
         return sum(1 for line in f)
 
-def write_to_output(row):
-    with open(OUTFILE, "a") as csvfile:
+def append_to_file(file, row):
+    with open(file, "a") as csvfile:
         csv.writer(csvfile, delimiter="|", lineterminator="\n", quotechar="~").writerow(row)
 
 if __name__ == "__main__":
-    num_songs_written = num_songs_processed()
+    num_songs_written = num_lines_in_file(OUTFILE)
     with open(INFILE) as csvfile:
         for idx, row in enumerate(csv.reader(csvfile, delimiter="|")):
             if (idx >= num_songs_written):
-                search_string = str.join(" ", row[0:2])
+                search_string = row[0] + " " + row[1]
                 row.append(get_youtube_id(search_string))
-                write_to_output(row)
+                append_to_file(OUTFILE, row)
