30300733af34a52b85427ad432bcdea977d339e2
Daniel Moore <hi@danielmoore.us>
2019-03-18 18:12:27 -0700

    maintain.bat now terminates on interruption of Python script.

--- maintain.bat
+++ maintain.bat
@@ -1,3 +1,5 @@
+@python random_acclaimed_song.py
+@if %errorlevel% neq 0 exit /b %errorlevel%
 @copy /y new-written written
 @call output-javascript.bat
 @copy /y songs.js ..\..\danielmoore.us\js\songs.js
