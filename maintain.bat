e267f56199594caa574a39efc152ee1a636067fc
Daniel Moore <hi@danielmoore.us>
2019-05-02 00:40:05 -0700

    maintenance

--- maintain.bat
+++ maintain.bat
@@ -2,6 +2,6 @@ rem schtasks /create /tn "random-acclaimed-song-maintenance" /tr C:\Users\hi\Des
 @wc -l C:\Users\hi\Desktop\code\random-acclaimed-song\new-written
 @python C:\Users\hi\Desktop\code\random-acclaimed-song\random_acclaimed_song.py
 @if %errorlevel% neq 0 exit /b %errorlevel%
-@copy /y new-written written
-@call output-javascript.bat
-@break > new-written
+@copy /y C:\Users\hi\Desktop\code\random-acclaimed-song\new-written C:\Users\hi\Desktop\code\random-acclaimed-song\written
+@call C:\Users\hi\Desktop\code\random-acclaimed-song\output-javascript.bat
+@break > C:\Users\hi\Desktop\code\random-acclaimed-song\new-written
