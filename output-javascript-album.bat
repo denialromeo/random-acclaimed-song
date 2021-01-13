fffb7c015dd4f3b0170ec6dd53dded6c9db12e85
Daniel Moore <hi@danielmoore.us>
2021-01-12 21:28:46 -0800

    more

--- output-javascript-album.bat
+++ output-javascript-album.bat
@@ -1,10 +1,9 @@
 @break > new-written
 @echo const albums = [ > albums.js
 @sed s/\"/\\\\\"/g albums > t
-@powershell "get-content .\t | set-content -encoding utf8 g"
-@sed "s/^\(.*\)$/\"\1\",/" g >> albums.js
+rem @powershell "get-content .\t | set-content -encoding utf8 g"
+@sed "s/^\(.*\)$/\"\1\",/" t >> albums.js
 @echo ] >> albums.js
 @del t
-@del g
 @copy /y albums.js C:\Users\hi\Desktop\danielmoore.us\js\albums.js
 @del *~
